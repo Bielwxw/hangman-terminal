f0356a0b0155757f096f877aa2f30d8dd3371c72
1anSantos <iann.silva2006@gmail.com>
2024-01-04 11:22:57 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const prompt = require('prompt-sync') ({sigint: false});
 const MenuInicial = require('./src/menuInicial');
+require('./src/config/index');
 
 const menu = new MenuInicial();
 menu.runMenu();
