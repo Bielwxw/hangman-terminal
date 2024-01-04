79dba2aeebf81d5dde5341db550e1e9996d0b765
Ian Santos <iann.silva2006@gmail.com>
2024-01-04 20:57:17 -0300

    colors added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// const prompt = require('prompt-sync') ({sigint: false});
-const MenuInicial = require('./src/menuInicial');
 require('./src/config/index');
 
+const colors = require('colors');
+
+colors.setTheme({
+  voltar: 'yellow',
+  menuBlue: ['blue', 'bold'],
+  menuGreen: ['green', 'bold'],
+  menuMagenta: ['magenta', 'bold'],
+  error: ['red', 'bold']
+});
+
+const MenuInicial = require('./src/menuInicial');
 const menu = new MenuInicial();
 menu.runMenu();
