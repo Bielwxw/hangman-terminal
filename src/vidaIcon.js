e8530ec164f2219c69f4a0862e0012600adadb19
1anSantos <iann.silva2006@gmail.com>
2024-01-05 10:02:28 -0300

    color

--- src/vidaIcon.js
+++ src/vidaIcon.js
@@ -1,25 +1,31 @@
 class VidaIcon {
   constructor(icone_vida = 'O', icone_vida_perdida = 'X') {
     this._habilitado = true;
-    this._iconeVida = icone_vida;
-    this._iconeVidaPerdida = icone_vida_perdida;
+    this._iconeVida = icone_vida.green;
+    this._iconeVidaPerdida = icone_vida_perdida.red;
   }
+
   toggleHabilitado() {
     const result = this.getHabilitado() ? false : true;
     this._habilitado = result;
   }
+
   getHabilitado() {
     return this._habilitado;
   }
+
   getIconVida() {
     return this._iconeVida;
   }
+
   getIconVidaPerdida() {
     return this._iconeVidaPerdida;
   }
+
   setIconVida(icon) {
     this._iconeVida = icon;
   }
+
   setIconVidaPerdida(icon) {
     this._iconeVidaPerdida = icon;
   }
